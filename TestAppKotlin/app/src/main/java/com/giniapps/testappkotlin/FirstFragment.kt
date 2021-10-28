a035206dea9333d138ac2e0918a0afa0f538bbe0
shay markovich <shaymark@gmail.com>
2021-10-28 14:43:07 +0300

    add Test app kotlin

---
+++ TestAppKotlin/app/src/main/java/com/giniapps/testappkotlin/FirstFragment.kt
@@ -0,0 +1,86 @@
+package com.giniapps.testappkotlin
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.giniapps.testappkotlin.databinding.FragmentFirstBinding
+import com.giniapps.testappkotlin.viewmodels.MainViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    val viewModel : MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // As collect is a suspend function, if you want to collect
+                // multiple flows in parallel, you need to do so in
+                // different coroutines
+                launch {
+                    viewModel.events.collect {
+                        Log.d(TAG, "onViewCreated: : $it")
+                    }
+                }
+
+                launch {
+                    viewModel.events.collect { /* Do something */ }
+                }
+            }
+        }
+
+
+        // **** if we use this we can see the event keep collecting also when the fragment is stop
+//        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+//            viewModel.events.collect {
+//                Log.d(TAG, "onViewCreated: : $it")
+//            }
+//        }
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    companion object {
+        val TAG = FirstFragment::class.simpleName
+    }
+}
---
+++ TestAppKotlin/app/src/main/java/com/giniapps/testappkotlin/viewmodels/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.giniapps.testappkotlin.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _events = MutableSharedFlow<String>() // private mutable shared flow
+    val events = _events.asSharedFlow() // publicly exposed as read-only shared flow
+
+    private suspend fun produceEvent(event: String) {
+        _events.emit(event)
+    }
+
+    init {
+        viewModelScope.launch {
+            var index = 0
+            while (true) {
+                produceEvent("new event" + index)
+                delay(200);
+                index += 1
+            }
+        }
+    }
+
+}
